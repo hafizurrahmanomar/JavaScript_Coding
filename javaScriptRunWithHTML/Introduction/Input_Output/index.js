87e129306c424ade0f639e0c523529c93df9f66e
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-05-25 19:16:10 +0600

    JavaScripts Re-arange all file and folder

--- javaScriptRunWithHTML/Introduction/Input_Output/index.js
+++
@@ -1,3 +0,0 @@
-let nameInput =prompt("Enter your name:");
-
-document.getElementById("output").innerHTML="Your name is "+ nameInput;
---
+++ javaScripts/objectLiterals/this.js
@@ -0,0 +1,9 @@
+const Hafiz={
+    fName: "Hafizur",
+    lName: "Rahman",
+    fullName: function(){
+        return this.fName + " " + this.lName;
+
+    }
+}
+Hafiz.fullName();
